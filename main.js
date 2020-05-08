432605771596e1e41b1621302542baecc3ff62c2
Karolina Wojciechowska <wojciechowska.k8@gmail.com>
2020-05-08 20:51:24 +0200

    prettier added

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const name = 'Karolina';
+const name = "Karolina";
 const age = 27;
 
-console.log('siemka');
+console.log("siemka");
 console.log(name);
 console.log(age);
-console.log(`Siemka, mam na imię ${name} i mam ${age} lat.`)
+console.log(`Siemka, mam na imię ${name} i mam ${age} lat.`);
