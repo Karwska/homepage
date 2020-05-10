efeaafac02740e0ef6777aa03c28ee0cdb980681
Karolina Wojciechowska <wojciechowska.k8@gmail.com>
2020-05-10 21:14:49 +0200

    sunday changes not finished yet

--- main.js
+++ main.js
@@ -5,3 +5,6 @@ console.log("siemka");
 console.log(name);
 console.log(age);
 console.log(`Siemka, mam na imię ${name} i mam ${age} lat.`);
+
+const headingTest = document.querySelector('.test-js');
+headingTest.innerHTML='To jest prawdziwa magia za pomocą JavaScriptu!'
